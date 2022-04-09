6d4bc7f3171ff2aaf70bebb20cf7de5fed5c75f6
AndyhSmith <andy.h.smith@gmail.com>
2022-04-08 20:28:31 -0600

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/sockets.io', (req, res) => {
+app.get('/explore', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
